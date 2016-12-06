71487bf3ce79732058ac6847df03e8f0dfb61fd4
Brisson Cyril <brissoncyril@yahoo.fr>
2016-12-06 16:11:46 +0100

    Create Instance.c

---
+++ Instance.c
@@ -0,0 +1,16 @@
+#include "instance.h"
+
+void initInstance(Instance *instance)
+{
+    instance->M=0;
+    instance->N=0;
+
+    instance->valueP=NULL;
+    instance->dimR=NULL;
+    instance->maxDimB=NULL;
+}
+
+void readFromFile(File *file)
+{
+
+}
