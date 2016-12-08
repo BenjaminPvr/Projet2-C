323dc94b9c59a5f67661ba8d7227b28c703bffc6
Brisson Cyril <brissoncyril@yahoo.fr>
2016-12-08 12:06:40 +0100

    Add files via upload

---
+++ Parser.c
@@ -0,0 +1,27 @@
+#include "Parser.h"
+#include "Instance.h"
+
+static char *getNextInformationFromFile(FILE *file);
+
+
+
+
+/**
+ *@brief cette fonction lit une instance depuis le fichier.
+ *@param file fichier dans lequel l'instance doit être lu.
+ *@return return the instance.
+ */
+
+void readInstanceFromFile(FILE *file)
+{
+    int N=0;
+    int M=0;
+    Instance *instance=NULL;
+
+
+}
+
+static char *getNextInformationFromFile(FILE *file)
+{
+   return "25";
+}
---
+++ Parser.h
@@ -0,0 +1,9 @@
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+#include "Instance.h"
+
+
+void readInstanceFromFile(FILE *file);
+
+#endif // PARSER_H_INCLUDED
