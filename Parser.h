c20dbb62da2434bae975a84a0e39ceb74c1b91cc
BenjaminPvr <pouvreau.benjamin@gmail.com>
2016-12-08 12:07:30 +0100

    Create Parser.h

---
+++ Parser.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+char * readLine(FILE * file);
+int * intTabFromFile(FILE * file, int numberOfInt);
+
+
+#endif /*PARSER_H_INCLUDED*/
