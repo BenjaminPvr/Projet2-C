406cb9289a6447d8b49c5f083f1145fd997f9a01
BenjaminPvr <pouvreau.benjamin@gmail.com>
2016-12-08 23:14:43 +0100

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -1,9 +1,14 @@
 #ifndef PARSER_H_INCLUDED
 #define PARSER_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "Instance.h"
 
 
+char * readLine(FILE * file);
+int * intTabFromFile(FILE * file, int numberOfInt);
 void readInstanceFromFile(FILE *file);
 
 #endif // PARSER_H_INCLUDED
