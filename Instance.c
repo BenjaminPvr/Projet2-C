ae4b4192f4ee0b3049cc7fd1b94db62a0e418a5b
Brisson Cyril <brissoncyril@yahoo.fr>
2016-12-08 12:07:17 +0100

    Add files via upload

--- Instance.c
+++ Instance.c
@@ -1,16 +1,29 @@
 #include "instance.h"
+#include "Error.h"
 
-void initInstance(Instance *instance)
+/**
+ *@brief Cette fonction initialise une instance de manière à ce qu'elle soit prête à être utilisée.
+ *@param  instance l'instance à initialisée.
+ *@warning Cette fonction doit être utilisée avant d'utiliser une instance .
+ */
+
+void initInstance(Instance *instance,const int N,const int M)
 {
-    instance->M=0;
-    instance->N=0;
+    instance->N=N;
+    instance->M=M;
 
-    instance->valueP=NULL;
-    instance->dimR=NULL;
-    instance->maxDimB=NULL;
+    instance->valueP=(double *)malloc(N*sizeof(double));
+    if(instance->valueP==NULL)
+        fatalError("Error : Allocation failed !");
+
+    instance->dimR=(double **)malloc(M*sizeof(double));
+    if(instance->dimR==NULL)
+        fatalError("Error : Allocation failed !");
+
+    instance->maxDimB=(double *)malloc(M*sizeof(double));
+    if(instance->maxDimB==NULL)
+        fatalError("Error : Allocation failed !");
 }
 
-void readFromFile(File *file)
-{
 
-}
+
--- Instance.h
+++ Instance.h
@@ -1,17 +1,24 @@
 #ifndef INSTANCE_H_INCLUDED
 #define INSTANCE_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct _Instance
 {
     int M;
     int N;
+    int number;
 
     int *valueP;
     int **dimR;
     int *maxDimB;
+
+    const char *name;
+
+
 }Instance;
 
-void initInstance(Instance *instance);
-void readFromFile(FILE *file);
+void initInstance(Instance *instance,const int M,const int N);
 
 #endif // INSTANCE_H_INCLUDED
